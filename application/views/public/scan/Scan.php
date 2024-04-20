f252037700a49d2744e0a6e9815a9844337cf5ce
sulthon1121 <sulthon.sdn@gmail.com>
2024-04-20 15:38:43 +0700

    scan before

--- application/views/public/scan/Scan.php
+++ application/views/public/scan/Scan.php
@@ -15,9 +15,29 @@
       <div class="mb-5 text-center text-uppercase">
         <h4>Scan</h4>
       </div>
-      <div>
-sdsd
+      <div class="row mb-5">
+        <div class="w-100 d-flex">
+          <div class="mx-auto">
+            <form action="">
+              <input type="text" name="" id="" placeholder="Input Qrcode">
+              <button type="submit">search</button>
+            </form>
+          </div>
+        </div>
       </div>
+      <!-- <div class="row mb-5 text-white" id="button-option" style="display:none;">
+        <div class="col-xl-6 col-sm-12 p-2" style="cursor:pointer;" onclick="window.open('keluar.com','_self')">
+          <div class="bg-secondary d-flex flex-column p-4 shadow">
+            <h3 class="m-auto mb-3">Keluar</h3>
+          </div>
+        </div>
+        <div class="col-xl-6 col-sm-12 p-2" style="cursor:pointer;" onclick="window.open('masuk.com','_self')">
+          <div class="bg-success d-flex flex-column p-4 shadow">
+            <h3 class="m-auto mb-3">Masuk</h3>
+          </div>
+        </div>
+      </div> -->
+      
     </div>
   </main>
 
