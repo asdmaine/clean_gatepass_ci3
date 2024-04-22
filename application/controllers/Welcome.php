e5742c8586fe5a7ffa2ef625be3bb9b294a76025
sulthon1121 <sulthon.sdn@gmail.com>
2024-04-23 02:30:56 +0700

    lang v2 (baru bisa ubah ubah)

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,36 +4,38 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Welcome extends CI_Controller
 {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		date_default_timezone_set('Asia/Jakarta');
+		$this->load->model('m_admin');
+		$language = $this->session->userdata('language');
+		$this->lang->load('general', $language);
+	}
 	public function index()
 	{
-		$this->lang->load('general','english');
-		echo $this->lang->line('welcome');
-		// $this->load->view('public/test/Lang');
+
+		$this->load->view('public/test/Lang');
 	}
 	public function show_pdf()
 	{
 		header("content-type: application/pdf");
 		readfile('src/assets/pdf/output.pdf');
 	}
-	public function mail(){
+	public function mail()
+	{
 		include APPPATH . 'third_party/phpmailer/src/Exception.php';
 		include APPPATH . 'third_party/phpmailer/src/PHPMailer.php';
 		include APPPATH . 'third_party/phpmailer/src/SMTP.php';
 		$this->load->view('public/test/test_mail');
+	}
+	function lang_change()
+	{
+		$language = $this->input->post('lan');
+		$this->session->set_userdata('language', $language);
+		$this->session->set_flashdata('suc', 'Successfully language Change at ' .
+			ucfirst($language));
+		echo '1';
+
 	}
 }
--- application/views/public/test/Lang.php
+++ application/views/public/test/Lang.php
@@ -6,11 +6,29 @@
     <title>Lang</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-   
+
 </head>
 
 <body>
-<?= $this->lang->line('welcome') ?>
+    <button style="cursor:pointer;" onclick="languagesChange('english')">English</button>
+    <button style="cursor:pointer;" onclick="languagesChange('indonesian')">Indonesian</button>
+
+    <?= $this->lang->line('welcome') ?>
 </body>
+
+<script src="<?php echo base_url('src/assets/js/jquery.min.js'); ?>"></script>
+<script>
+    function languagesChange(lan) {
+        $.ajax({
+            type: "POST",
+            url: "<?php echo base_url("welcome/lang_change") ?>",
+            data: { 'lan': lan },
+            success: function (data) {
+                location.reload();
+            }
+        });
+    }
+
+</script>
 
 </html>
